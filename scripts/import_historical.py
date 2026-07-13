eb9c6cc2178a272af1c10343499eaf1897717dbb
Alain Mimeault <lhorde@msn.com>
2026-07-13 22:13:26 +0900

    Finish writing importer file for large datasets

--- scripts/import_historical.py
+++ scripts/import_historical.py
@@ -1,35 +1,78 @@
 from pathlib import Path
+
 import pandas as pd
+import psycopg
+import io
 
 DATA_FILE = Path("data/nq-1m_bk.csv")
 
-df = pd.read_csv(
-    DATA_FILE,
-    sep=";",
-    header=None,
-    names=[
-        "Date",
-        "Time",
-        "Open",
-        "High",
-        "Low",
-        "Close",
-        "Volume"
-    ],
-    skiprows=5_883_000,
-    nrows=2000,
-)
+with psycopg.connect("dbname=dailyedge_development") as connection:
+    print("Connected to PostgreSQL")
 
-df["Timestamp"] = pd.to_datetime(
-    df["Date"] + " " + df["Time"],
-    format="%d/%m/%Y %H:%M"
-)
+    total_rows = 0
 
-day = df[
-    (df["Timestamp"] >= "2026-06-30")
-    & (df["Timestamp"] < "2026-07-01")
-]
+    for chunk in pd.read_csv(
+        DATA_FILE,
+        sep=";",
+        header=None,
+        names=[
+            "Date",
+            "Time",
+            "Open",
+            "High",
+            "Low",
+            "Close",
+            "Volume",
+        ],
+        skiprows=5_800_000,
+        chunksize=100_000,
+    ):
+        chunk["Timestamp"] = pd.to_datetime(
+            chunk["Date"] + " " + chunk["Time"],
+            format="%d/%m/%Y %H:%M"
+        )
 
-print(len(day))
-print(day.head())
-print(day.tail())
+        chunk = chunk.drop_duplicates(subset=["Timestamp"])
+
+        duplicates = chunk[chunk["Timestamp"].duplicated(keep=False)]
+
+        if not duplicates.empty:
+            print(duplicates)
+
+        chunk = chunk.drop(columns=["Date", "Time"])
+
+        chunk = chunk[
+            [
+                "Timestamp",
+                "Open",
+                "High",
+                "Low",
+                "Close",
+                "Volume",
+            ]
+        ]
+
+        buffer = io.StringIO()
+        chunk.to_csv(buffer, index=False, header=False)
+        buffer.seek(0)
+
+        with connection.cursor() as cursor:
+            with cursor.copy(
+                """
+                COPY candles (
+                    timestamp,
+                    open,
+                    high,
+                    low,
+                    close,
+                    volume
+                )
+                FROM STDIN WITH (FORMAT CSV)
+                """
+            ) as copy:
+                copy.write(buffer.read())
+        connection.commit()
+
+
+        total_rows += len(chunk)
+        print(f"Imported {total_rows:,} rows...")
